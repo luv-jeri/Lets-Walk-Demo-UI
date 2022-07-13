3945bfa06ea815f8f32a25a84ddd1b4009cb81a0
luv-jeri <luvvjeri@gmail.com>
2022-07-13 22:34:48 +0530

    -

--- src/App.js
+++ src/App.js
@@ -1,24 +1,91 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
+import { TextInput, Container, Text, Button } from '@mantine/core';
+import axios from 'axios';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const login = async () => {
+    try {
+      const { data } = await axios.post(
+        'http://localhost:8000/api/v1/auth/sign_in',
+        {
+          email,
+          password,
+        },
+        {
+          withCredentials: true,
+        }
+      );
+
+      localStorage.setItem('token', data.token);
+
+      console.log(data);
+    } catch (e) {
+      console.log(e.message);
+    }
+  };
+
+  const getTours = async () => {
+    try {
+      const { data } = await axios.get('http://localhost:8000/api/v1/tours', {
+        withCredentials: true,
+        headers: {
+          'Content-Type': 'application/json',
+          Authorization: `Bearer ${localStorage.getItem('token')}`,
+        },
+      });
+      console.log(data);
+    } catch (e) {
+      console.log(e.response.data);
+    }
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Container
+      style={{
+        height: '30vh',
+        width: '50vw',
+        display: 'flex',
+        flexDirection: 'column',
+        justifyContent: 'space-around',
+        alignItems: 'stretch',
+      }}
+    >
+      <Text size='xl' weight={700}>
+        LOGIN
+      </Text>
+      <TextInput
+        placeholder='Email Address'
+        required
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+      <TextInput
+        placeholder='Password'
+        type='password'
+        required
+        onChange={(e) => {
+          setPassword(e.target.value);
+        }}
+      />
+      <Button
+        variant='gradient'
+        gradient={{ from: 'indigo', to: 'cyan' }}
+        onClick={login}
+      >
+        Login
+      </Button>
+      <Button
+        variant='gradient'
+        gradient={{ from: 'indigo', to: 'cyan' }}
+        onClick={getTours}
+      >
+        GET TOURS
+      </Button>
+    </Container>
   );
 }
 
